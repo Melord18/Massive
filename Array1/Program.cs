189ef25764f88e818d4fadd8f0649b1b4d691d4d
Aziz <samadovaziz90@gmail.com>
2023-04-05 07:37:38 +0500

    Arrray2

--- Array1/Program.cs
+++ Array1/Program.cs
@@ -26,25 +26,37 @@
           
             for (int i = 0; i < first; i++)
             {
+                array[i] = new int[second];
+
                 for (int j = 0; j < second; j++)
                 {
                     array[i][j]  = random.Next(0,100);
+                   
+                }
+            }
+             
+            for(int i = 0; i < first;i++)
+            {
+                for     (int j = 0;j < second; j++)
+                {
                     if (max <= array[i][j])
                     {
                         max = array[i][j];
-                        Console.WriteLine("Max number in massive : " + max);
+                        
                     }
                     if (min >= array[i][j])
                     {
                         min = array[i][j];
-                        Console.WriteLine("Min number in massive : " + min);
-                        Console.WriteLine();
+                        
                     }
                 }
             }
-            
-            
-           }
+            Console.WriteLine("Max number in massive : " + max);
+            Console.WriteLine("Min number in massive : " + min);
+            Console.WriteLine();
+
+
+        }
     
         
     }
